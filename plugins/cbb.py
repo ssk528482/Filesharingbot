61c5f70b2e2b98c54db7ea70b7df0543903bbd64
Ajin <103480559+Jungkook190@users.noreply.github.com>
2023-04-21 13:17:35 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/CCHDMovie'>Click here</a>\n○ Channel : @cinemala_com1\n○ Channel : @CinemaShopLinkz</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/C5CHD'>Click here</a>\n○ Channel : @cinemala_com1\n○ Channel : @CinemaShopLinkz</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
